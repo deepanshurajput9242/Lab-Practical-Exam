2de90aa97c2f05d044d5d7083c94a7018c35c763
deepanshurajput9242 <imdeepanshu1rajpoot@gmail.com>
2026-05-05 12:15:57 +0530

    Initialize Express server in app.js

---
+++ authorisation_app/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const routes = require('./routes/routes');
+
+
+app.use('/', routes);
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
